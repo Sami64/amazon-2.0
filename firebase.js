11f958a1fe2c015ac7495012590b30e6b23f7db6
Samuel Kow Paintsil <smashbros54@gmail.com>
2021-06-01 14:11:44 +0000

    🚀  checkout build

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSrQp2HdV7ysIr7KRwj1U_6YdXN-lGWFw",
+  authDomain: "next-22aec.firebaseapp.com",
+  projectId: "next-22aec",
+  storageBucket: "next-22aec.appspot.com",
+  messagingSenderId: "28457988040",
+  appId: "1:28457988040:web:de099c0b483a6477989afd",
+  measurementId: "G-300FGFJVZK",
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import { Provider as AuthProvider } from "next-auth/client";
+import { store } from "../app/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider session={pageProps.session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
